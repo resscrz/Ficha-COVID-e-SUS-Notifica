ec43e75deb7cfe01dd3c02bbc120a4bc78f4e94e
Ramon Salazar <resscrz@gmail.com>
2021-10-22 07:58:14 -0300

    Update
    Pag1

--- src/pag1campo.java
+++ src/pag1campo.java
@@ -5,16 +5,7 @@ import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
 import java.io.FileOutputStream;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author RESSCRZ
- */
 public class pag1campo {
  
     public void campos1(){
